91afb5a51d289344c3a226307e7237726aef2952
Thomas Robinson <git@tomr.me>
2023-01-24 16:51:03 +0000

    add stuff from today

--- test.py
+++ test.py
@@ -1,12 +1,32 @@
 from djitellopy import Tello
+import time
 
 tello = Tello()
 
 tello.connect()
-tello.takeoff()
 
-tello.move_left(100)
-tello.rotate_counter_clockwise(90)
-tello.move_forward(100)
+grid = list()
 
-tello.land()
+def fetchHeight():
+	return str(tello.get_distance_tof())
+
+print("battery pct " + str(tello.get_battery()))
+
+# tello.takeoff()
+# i = 0
+
+# x = 0
+# y = 0
+# x_max = 10
+# y_max = 10
+
+# for cx in range(x_max):
+# 	for cy in range (y_max):
+
+# 		print(str(cx) + "," + str(cy) + " " + fetchHeight())
+# 		time.sleep(2)
+
+
+while True:
+	print(fetchHeight())
+	time.sleep(0.5)
